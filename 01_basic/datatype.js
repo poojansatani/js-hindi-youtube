dcc85f5190690eae00b59c246cb24abb85807ba0
poojansatani <138504193+poojansatani@users.noreply.github.com>
2024-12-10 14:25:46 +0000

    comparision

--- 01_basic/datatype.js
+++ 01_basic/datatype.js
@@ -1,21 +1,65 @@
 "use strict"; //treat all js code as newer version
 //alert(3+3)// we are using node js not browser
  let score=null
- console.log(typeof score);
+ //console.log(typeof score);
  let vn= Number(score)
- console.log(typeof vn);
- console.log(vn);
+ //console.log(typeof vn);
+ //console.log(vn);
+
  //"33"=> 33
  //"3abc"=> NaN
 // true=>1
 // null => object 
 
 let isloggedin=""
- console.log(typeof score);
+ //console.log(typeof score);
  let bn= Boolean(score)
- console.log(typeof bn);
- console.log(bn);
+ //console.log(typeof bn);
+ //console.log(bn);
 let someNumber="ab"
 let stringNumber = String (someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+let value=3
+let negvalue= -value
+//console.log(negvalue);
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "poojan"
+
+let str3 = str1 + str2
+// console.log(str3);
+// console.log("1"+2);
+// console.log(2+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+// console.log(true);
+ //console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3 = 2+2
+
+let gamecounter=100
+gamecounter++;
+//console.log(gamecounter);
+// console.log("2">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined<0);
+
+// console.log("2"==2);
+
