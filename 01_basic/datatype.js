bea1b889d0dc64de199d773277966af3d55695a0
poojansatani <138504193+poojansatani@users.noreply.github.com>
2024-12-10 13:47:14 +0000

    conversion of data type

---
+++ 01_basic/datatype.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3)// we are using node js not browser
+ let score=null
+ console.log(typeof score);
+ let vn= Number(score)
+ console.log(typeof vn);
+ console.log(vn);
+ //"33"=> 33
+ //"3abc"=> NaN
+// true=>1
+// null => object 
+
+let isloggedin=""
+ console.log(typeof score);
+ let bn= Boolean(score)
+ console.log(typeof bn);
+ console.log(bn);
+let someNumber="ab"
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
