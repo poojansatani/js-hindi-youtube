6ab89a1ed44c412e7ad5d571c3ad384ea6fc483f
poojansatani <138504193+poojansatani@users.noreply.github.com>
2024-12-11 08:43:10 +0000

    datasummary

---
+++ 01_basic/datatypesummary.js
@@ -0,0 +1,22 @@
+ //7 types:string,Number,boolean,null,undefined,symbol,big Int
+ const score=100
+ const scorevalue=100.3
+ const isloggedIn= false
+ const outside=null
+ let useremail;
+ const id =Symbol('123')
+ const anotherid = Symbol('123')
+ //const bignumber = 565464844884895984976874684n
+ const heros = ["shaktiman","naagraj","dogga"];
+ let myobj={
+    name:"hitesh",
+    age:22,
+ }
+
+ const myfunction = function(){
+    console.log("helloworld");
+ }
+ console.log( myobj.name);
+ // Refrence (Non  primitive)
+ //array,objects,Functions
+ 
